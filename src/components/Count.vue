8de2d6e5c543eceaee59ce847d9d1cf3b60f2c22
sqq321 <2463127380@qq.com>
2019-08-24 15:24:04 +0800

    'lesson-1'

---
+++ src/components/Count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="count">
+    <div>
+      <hr />
+      <h1>Count.vue:{{count}}</h1>
+      <p>{{todos}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "count",
+  components: {},
+  computed: mapState(["count", "todos"]),
+  computed: {
+    count() {
+      return this.$store.state.count; //main.js中的store
+    },
+    todos() {
+      return this.$store.state.todos;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    //存储属性
+    count: 0,
+    todos: [
+      { id: 1, tilte: "todo item 1", completed: false },
+      { id: 2, tilte: "todo item 2", completed: false },
+      { id: 3, tilte: "todo item 3", completed: false },
+    ]
+  },
+  getters: {
+    //获取属性
   },
   mutations: {
 
